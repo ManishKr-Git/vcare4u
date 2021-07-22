a6b36764d6cc1b1d0842cd0206d16034ad9da745
ManishKr-Git <manishkumarchoumiyan1998@gmail.com>
2021-07-22 22:07:17 +0530

    Updated

--- src/services/constant.js
+++ src/services/constant.js
@@ -1,7 +1,7 @@
-// export const BASE_URL = "https://vcare4u-springboot.herokuapp.com";
-export const BASE_URL = "http://localhost:8080";
-export const LOCAL_URL = "http://localhost:3000";
-// export const LOCAL_URL = "https://vcare4u-uoh.herokuapp.com";
+export const BASE_URL = "https://vcare4u-springboot.herokuapp.com";
+// export const BASE_URL = "http://localhost:8080";
+// export const LOCAL_URL = "http://localhost:3000";
+export const LOCAL_URL = "https://vcare4u-uoh.herokuapp.com";
 function importAll(r) {
   return r.keys().map(r);
 }
